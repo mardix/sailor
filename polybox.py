08fc41ba3f45f6235688568ae2b34cd390cb4f8a
Mardix <mardix@pylot.io>
2022-01-21 03:37:33 -0500

    1.1.0

--- polybox.py
+++ polybox.py
@@ -42,7 +42,7 @@ from grp import getgrgid
 # -----------------------------------------------------------------------------
 
 NAME = "Polybox"
-VERSION = "1.1.0-b.04" 
+VERSION = "1.1.0" 
 VALID_RUNTIME = ["python", "node", "static", "shell"]
 
 
